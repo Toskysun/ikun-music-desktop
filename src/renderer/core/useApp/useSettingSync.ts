de1911023b628dc83c95888539c3b6b5c13c249b
Folltoshe <folltoshe@foxmail.com>
2025-05-10 10:43:58 +0800

    更新代理配置规则，现在不启用代理时，图片、音频加载将不再走系统代理 (#2382)
    * 完善代理设置
    
    * 更新
    
    * update
    
    ---------
    
    Co-authored-by: lyswhut <lyswhut@qq.com>

--- src/renderer/core/useApp/useSettingSync.ts
+++ src/renderer/core/useApp/useSettingSync.ts
@@ -22,122 +22,13 @@ export default () => {
     }
   )
 
-  watch(
-    () => appSetting['common.langId'],
-    (id) => {
-      if (!id) return
-      setLanguage(id)
-      window.setLang(id)
-    }
-  )
-
-  watch(
-    () => appSetting['common.apiSource'],
-    (apiSource) => {
-      void setUserApi(apiSource)
-    }
-  )
-
-  watch(
-    () => appSetting['common.font'],
-    (val) => {
-      document.documentElement.style.fontFamily = val
-    },
-    {
-      immediate: true,
-    }
-  )
-
-  watch(
-    () => appSetting['sync.mode'],
-    (mode) => {
-      sync.mode = mode
-    }
-  )
-
-  watch(
-    () => appSetting['sync.enable'],
-    (enable) => {
-      switch (appSetting['sync.mode']) {
-        case 'server':
-          if (appSetting['sync.server.port']) {
-            void sendSyncAction({
-              action: 'enable_server',
-              data: {
-                enable: appSetting['sync.enable'],
-                port: appSetting['sync.server.port'],
-              },
-            }).catch((err) => {
-              console.log(err)
-            })
-          }
-          break
-        case 'client':
-          if (appSetting['sync.client.host']) {
-            void sendSyncAction({
-              action: 'enable_client',
-              data: {
-                enable: appSetting['sync.enable'],
-                host: appSetting['sync.client.host'],
-              },
-            }).catch((err) => {
-              console.log(err)
-            })
-          }
-          break
-        default:
-          break
-      }
-      sync.enable = enable
-    }
-  )
-  watch(
-    () => appSetting['sync.server.port'],
-    (port) => {
-      if (appSetting['sync.mode'] == 'server') {
-        void sendSyncAction({
-          action: 'enable_server',
-          data: {
-            enable: appSetting['sync.enable'],
-            port: appSetting['sync.server.port'],
-          },
-        })
-      }
-      sync.server.port = port
-    }
-  )
-  watch(
-    () => appSetting['sync.client.host'],
-    (host) => {
-      if (appSetting['sync.mode'] == 'client') {
-        void sendSyncAction({
-          action: 'enable_client',
-          data: {
-            enable: appSetting['sync.enable'],
-            host: appSetting['sync.client.host'],
-          },
-        })
-      }
-      sync.client.host = host
-    }
-  )
-
-  watch(
-    () => appSetting['network.proxy.enable'],
-    (enable) => {
-      proxy.enable = enable
-    }
-  )
-  watch(
-    () => appSetting['network.proxy.host'],
-    (host) => {
-      proxy.host = host
-    }
-  )
-  watch(
-    () => appSetting['network.proxy.port'],
-    (port) => {
-      proxy.port = port
-    }
-  )
+  watch(() => appSetting['network.proxy.enable'], enable => {
+    proxy.enable = enable
+  })
+  watch(() => appSetting['network.proxy.host'], host => {
+    proxy.host = host
+  })
+  watch(() => appSetting['network.proxy.port'], port => {
+    proxy.port = port
+  })
 }
