c57084aebdeb8dca9e78fb8f9862cc941a68766b
ikun0014 <ikun0014@qq.com>
2025-07-04 12:53:30 +0800

    重新提交

---
+++ .ncurc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  upgrade: true,
+  reject: [
+    'electron',
+    'chalk',
+    'del',
+    'comlink',
+    'vue',
+    'image-size',
+    'message2call',
+    '@types/ws',
+    '@types/node',
+    'electron-debug',
+  ],
+}
---
+++ src/renderer/utils/musicSdk/api-source-info.ts
@@ -0,0 +1,10 @@
+const sources: Array<{
+  id: string
+  name: string
+  disabled: boolean
+  supportQualitys: Partial<Record<LX.OnlineSource, LX.Quality[]>>
+}> = [
+
+]
+
+export default sources
